b74cb73fdc62196316a0fdede8eeaf47b5c8207b
amandeep-singh-167 <9eamandeepbis.singh@gmail.com>
2026-03-04 23:37:02 +0530

    Add files via upload

---
+++ code1.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+#include <Adafruit_LiquidCrystal.h>
+
+//int seconds = 0;
+
+Adafruit_LiquidCrystal lcd(0);
+
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.setBacklight(1);
+  
+}
+
+void loop()
+{
+
+  lcd.setCursor(0, 0);
+  lcd.print("I LOVE YOU");
+  // lcd.setCursor(0, 1);
+  //lcd.print(seconds);
+   //delay(1000);
+  //seconds += 1;
+}
